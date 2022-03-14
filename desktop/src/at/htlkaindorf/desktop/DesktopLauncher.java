607ab13e0a0ec5f402ab2083d7fd912e67863464
trunia18 <nik.trummer16.nt@gmail.com>
2022-03-14 11:32:12 +0100

    Project created

---
+++ desktop/src/at/htlkaindorf/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package at.htlkaindorf.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import at.htlkaindorf.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
