749c4b601b1fb0eb04bb8665d8cf7ad5febdf66b
Manuel Reicht <86600051+ManuReicht@users.noreply.github.com>
2022-03-21 14:10:01 +0100

    added seasonal tileset

---
+++ core/assets/maps/Seasonal_Grassland.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Seasonal_Grassland" tilewidth="16" tileheight="16" tilecount="470" columns="47">
+ <image source="Seasonal Tilesets/1 - Grassland/to8le-6ru5h.png" width="752" height="160"/>
+</tileset>
