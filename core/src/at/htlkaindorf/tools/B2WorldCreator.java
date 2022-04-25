29535a04097dec158544e3e3e9d7f63805476f02
trunia18 <nik.trummer16.nt@gmail.com>
2022-04-25 14:27:53 +0200

    Update B2WorldCreator.java

--- core/src/at/htlkaindorf/tools/B2WorldCreator.java
+++ core/src/at/htlkaindorf/tools/B2WorldCreator.java
@@ -1,15 +1,18 @@
 package at.htlkaindorf.tools;
 
 import at.htlkaindorf.Game;
+import at.htlkaindorf.entities.enemies.Walker;
 import at.htlkaindorf.screens.PlayScreen;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.utils.Array;
 
 
 public class B2WorldCreator {
+    private Array<Walker> walkers;
 
     public B2WorldCreator(PlayScreen screen){
         World world = screen.getWorld();
@@ -36,20 +39,12 @@ public class B2WorldCreator {
             body.createFixture(fdef);
         }
 
-        //create pipe bodies/fixtures
-        for(MapObject object : map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)){
+        //create enemies
+        walkers = new Array<Walker>();
+        for(MapObject object : map.getLayers().get(6).getObjects().getByType(RectangleMapObject.class)){
             Rectangle rect = ((RectangleMapObject) object).getRectangle();
-
-            bdef.type = BodyDef.BodyType.StaticBody;
-            bdef.position.set((rect.getX() + rect.getWidth() / 2) / Game.getInstance().getPPM(),
-                              (rect.getY() + rect.getHeight() / 2) / Game.getInstance().getPPM());
-
-            body = world.createBody(bdef);
-
-            shape.setAsBox(rect.getWidth() / 2 / Game.getInstance().getPPM(),
-                           rect.getHeight() / 2 / Game.getInstance().getPPM());
-            fdef.shape = shape;
-            body.createFixture(fdef);
+            walkers.add(new Walker(screen, rect.getX() / Game.getInstance().getPPM(),
+                    rect.getY() / Game.getInstance().getPPM()));
         }
     }
 }
