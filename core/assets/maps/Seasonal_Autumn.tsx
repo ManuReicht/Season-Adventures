5528f6097d013f545a460b3fbcfcf97bfa2d54cd
Manuel Reicht <86600051+ManuReicht@users.noreply.github.com>
2022-05-23 10:55:06 +0200

    added seasonal tilesets

---
+++ core/assets/maps/Seasonal_Autumn.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Seasonal_Autumn" tilewidth="16" tileheight="16" tilecount="580" columns="20">
+ <image source="Seasonal Tilesets/Seasonal_Autumn.png" width="320" height="464"/>
+</tileset>
